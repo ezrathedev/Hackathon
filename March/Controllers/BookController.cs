e65445fa4f896c8d58a2c062126fcf19d43f6671
sarah matta <sarah.matta@enformion.com>
2023-03-03 17:58:52 -0500

    initial backend

---
+++ March/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+namespace DefaultNamespace;
+
+public class BookController
+{
+    public async NeighborhoodPage()
+    {
+        
+    }
+    
+    public async AnimalFriendlyPage()
+    {   
+        
+    }
+    
+    public async CommunitySupportPage()
+    {
+        
+    }
+    
+    public async FoodPage()
+    {
+        
+    }
+    
+    public async GreenSpacesService()
+    {
+        
+    }
+    public async WalkBikePage()
+    {
+        
+    }
+    public async HousesPage()
+    {
+        
+    }
+}
---
+++ March/Services/HousesService.cs
@@ -0,0 +1,6 @@
+namespace DefaultNamespace;
+
+public class HousesService
+{
+    //Huge pantry & 2 fridges
+}
